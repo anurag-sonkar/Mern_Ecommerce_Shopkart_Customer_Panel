1baa95f7ad603009cd5de46cd8a542e7ad61cf03
Anurag <anuragsonkar053@gmail.com>
2024-07-27 21:22:41 +0530

    compareProduct-route-and-compareCard-done

---
+++ src/components/CompareCard.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Typography,
+} from "@material-tailwind/react";
+
+import RatingMUI from "./RatingMUI";
+import { IoCartOutline } from "react-icons/io5";
+import { FaRegEye } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
+
+function CompareCard({ product }) {
+  const info = product.info;
+  console.log(info);
+  return (
+    <Card className="lg:w-64 relative group flex lg:flex-col flex-row">
+      <CardHeader shadow={false} floated={false} className="h-56">
+        <img
+          src={product.imageSrc}
+          alt={product.imageAlt}
+          className="h-full w-full object-cover group-hover:scale-110 transition-all duration-400"
+        />
+
+        <div className="absolute top-2 lg:-right-6 right-1 grid gap-3 transition-all duration-300 cursor-pointer group-hover:right-1">
+          <div className="bg-orange-600 hover:bg-red-600 text-gray-950 font-semibold rounded-full grid place-items-center lg:p-1 md:p-1 p-0 transition-all duration-300 ">
+            <IoClose color="black" className="hover:scale-125" />
+          </div>
+          <div className="bg-orange-600 hover:bg-red-600 text-gray-950 font-semibold rounded-full lg:grid place-items-center p-1 transition-all duration-300 hidden">
+            <FaRegEye color="black" className="hover:scale-125" />
+          </div>
+          <div className="bg-orange-600 hover:bg-red-600 text-gray-950 font-semibold rounded-full lg:grid place-items-center p-1 transition-all duration-300 hidden">
+            <IoCartOutline color="black" className="hover:scale-125" />
+          </div>
+        </div>
+      </CardHeader>
+
+      <CardBody>
+        <div>
+          <Typography color="black" className="font-semibold">
+            {product.name}
+          </Typography>
+        </div>
+
+        <div className="mb-1 flex items-center justify-between">
+          <Typography color="blue-gray" className="font-medium">
+            Price
+          </Typography>
+          <div className="flex gap-2">
+            <Typography className="font-semibold text-red-400">
+              {product.price}
+            </Typography>
+            <Typography className="font-medium text-gray-500 line-through">
+              {product.discountedPrice}
+            </Typography>
+          </div>
+        </div>
+
+        <div>
+          {/* Other product details */}
+          <div>
+            {product.info &&
+              Object.keys(product.info).map((key) => {
+                if (key === "color") {
+                  return (
+                    <div key={key} className="flex justify-between capitalize">
+                      <div className="font-semibold">{key}</div>
+                      <div className="flex space-x-1 items-center">
+                        {product.info[key].map((color) => (
+                          <div
+                            key={color}
+                            className="lg:w-5 lg:h-5 h-3 w-3 rounded-full"
+                            style={{ backgroundColor: color }}
+                          ></div>
+                        ))}
+                      </div>
+                    </div>
+                  );
+                } else {
+                  return (
+                    <div key={key} className="flex justify-between capitalize lg:gap-0 md:gap-16 gap-6">
+                      <div className="font-semibold">{key}</div>
+                      <div className="min-w-16 text-right">{product.info[key]}</div>
+                    </div>
+                  );
+                }
+              })}
+          </div>
+        </div>
+      </CardBody>
+    </Card>
+  );
+}
+
+export default CompareCard;
--- src/pages/CompareProducts.jsx
+++ src/pages/CompareProducts.jsx
@@ -1,13 +1,118 @@
 import React from 'react'
 import HelmetTitle from "../components/HelmetTitle"
 import {BreadCrumb} from "../components/BreadCrumb"
+import CompareCard from '../components/CompareCard'
+
+const products = [
+  {
+    id: 1,
+    name: "Basic Tee",
+    price: "$35",
+    discountedPrice : "$22",
+    href: "#",
+    imageSrc:
+    "https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80",
+    
+    imageAlt: "Front of men's Basic Tee in black.",
+    info:{
+    type:"Electronics",
+    color: ["#586952","#458520" , "yellow" , "#658520" , "skyblue"],
+    brand:"Havells",
+    SKU: "SKU033",
+    Size: ["S" , "M" , "L"],
+    Availability : "In Stock",
+    ratings : 4
+    }
+  },
+  {
+    id: 2,
+    name: "Tablet",
+    href: "#",
+    imageSrc: "../src/assets/tab.jpg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+    discountedPrice:"$30",
+    info:{
+    type:"Electronics",
+    color: ["crimson","#458520" , "#658520"],
+    brand:"Havells",
+    SKU: "SKU033",
+    Size: ["S" , "M" , "L"],
+    Availability : "In Stock"
+    }
+  },
+  {
+    id: 3,
+    name: "Basic Tee",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+    price: "$35",
+    discountedPrice :"$26",
+    info:{
+    type:"Electronics",
+    color: ["#125685","#458520" , "#658520"],
+    brand:"Havells",
+    SKU: "SKU033",
+    Size: ["S" , "M" , "L"],
+    Availability : "In Stock"
+    }
+  },
+  {
+    id: 4,
+    name: "Basic Tee",
+    href: "#",
+    imageSrc:
+      "https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+    discountedPrice:"$28",
+    info:{
+    type:"Electronics",
+    color: ["#125685","#458520" , "#658520"],
+    brand:"Havells",
+    SKU: "SKU033",
+    Size: ["S" , "M" , "L"],
+    Availability : "In Stock"
+    }
+  },
+  {
+    id: 5,
+    name: "Basic Tee",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+    discountedPrice:"$28",
+    info:{
+    type:"Electronics",
+    color: ["#125685","#458520" , "#658520"],
+    brand:"Havells",
+    SKU: "SKU033",
+    Size: ["S" , "M" , "L"],
+    Availability : "In Stock"
+    }
+  },
+  // More products...
+];
 function CompareProducts() {
   return (
     <div>
 
-      <HelmetTitle title="Contact" />
+      <HelmetTitle title="Compare Products" />
       <div className="bg-white lg:px-10 px-5 py-1 flex justify-between items-center">
-        <BreadCrumb title="Contact" />
+        <BreadCrumb title="Compare Products" />
+      </div>
+
+      <div className='lg:px-10 grid lg:grid-cols-4 md:grid-cols-2 place-items-center gap-4 lg:py-12 '>
+      {products.map((product) => (
+              <div key={product.id} className="group relative">
+                {" "}
+                <CompareCard product={product} />
+              </div>
+      ))}
+
       </div>
     </div>
   )
